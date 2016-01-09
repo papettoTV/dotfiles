e89903c1e0b78d40d4f17f35fefdc9e50fcc7ad1
papettoTV <aegfrompsbt+test@gmail.com>
2016-01-09 19:46:50 +0900

    add init_update.sh

---
+++ init_update.sh
@@ -0,0 +1,2 @@
+echo "let g:vimrc_local_finish = 1" > /Applications/MacVim.app/Contents/Resources/vim/vimrc_local.vim; \
+echo "let g:gvimrc_local_finish = 1" > /Applications/MacVim.app/Contents/Resources/vim/gvimrc_local.vim
